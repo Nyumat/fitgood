02f7e3866d072bdb8f2dcf6fc9d5fa3db60fa0e7
Thomas <nyumat18@gmail.com>
2022-10-09 02:49:52 -0700

    Connected Frontend to Backend - Categories & Upload Item

--- client/screens/FitFactory.js
+++ client/screens/FitFactory.js
@@ -1,216 +1,218 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import { Select, Center, Box, CheckIcon, Button } from "native-base";
-import { ScrollView } from "react-native-gesture-handler";
 
 export default function FitFactory() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        style={{
-          fontSize: 30,
-          fontWeight: "bold",
-          color: "black",
-          textAlign: "center",
-          top: 90,
-          position: "absolute",
-          fontFamily: "Apple SD Gothic Neo"
-        }}
-      >
-        FitFactory
-      </Text>
-      <Center>
-        <Button style={{ 
-          position: "absolute",
-          top: 200, 
-          width: 300,
-          height:40,
-          lineheight: 34, 
-          backgroundColor: "#D3D3D3",
-          borderradius: 69,
-          fontWeight: 500,
-          fontSize: 40,           
-          textAlign: "center",
-          color: "#000000",
-               }}>
-          <Text style={{fontFamily: "Apple SD Gothic Neo",}}>Generate Fit</Text>
-        </Button>
-        <Button style={{ 
-          position: "absolute",
-          top: 250, 
-          width: 300,
-          height:40,
-          lineheight: 34, 
-          backgroundColor: "#D3D3D3",
-          borderradius: 69,
-          fontWeight: 500,
-          fontSize: 40,           
-          textAlign: "center",
-          color: "#000000",
-               }}>
-          <Text style={{fontFamily: "Apple SD Gothic Neo",}}>Add to Favorites</Text>
-        </Button>
-      </Center>
-
-        <View
-          style={{
-
-            position: "absolute",
-
-            top: 180,
-            width: 350,
-            height: 350,
-            borderRadius: 20,
-          
-            backgroundColor: "#E2ECD0",
-            shadowColor: "#000",
-        }}
-      >
-        {/* Four squarees */}
-        <View
-          style={{
-            backgroundColor: "white",
-            position: "absolute",
-            top: 10,
-            left: 10,
-            width: 160,
-            height: 160,
-            borderRadius: 20,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-
-            elevation: 5,
-          }}
-        >
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text
-            style={{
-              fontSize: 20,
-              fontWeight: "bold",
-              color: "black",
-              textAlign: "center",
-              marginLeft: "25%",
-              top: 50,
-              position: "absolute",
-              fontFamily: "Apple SD Gothic Neo",
-            }}
+              style={{
+                  fontSize: 40,
+                  fontWeight: 'bold',
+                  color: 'black',
+                  textAlign: 'center',
+                  top: 90,
+                  position: 'absolute',
+                  fontFamily: 'Apple SD Gothic Neo',
+              }}
           >
-            Top Left
+              FitFactory
           </Text>
-        </View>
+          <Center>
+              <Button
+                  style={{
+                      position: 'absolute',
+                      top: 200,
+                      width: 300,
+                      height: 40,
+                      lineheight: 34,
+                      backgroundColor: '#D3D3D3',
+                      borderRadius: 69,
+                      fontWeight: 500,
+                      fontSize: 40,
+                      textAlign: 'center',
+                      color: '#000000',
+                  }}
+              >
+                  <Text style={{ fontFamily: 'Apple SD Gothic Neo' }}>Generate Fit</Text>
+              </Button>
+              <Button
+                  style={{
+                      position: 'absolute',
+                      top: 250,
+                      width: 300,
+                      height: 40,
+                      lineheight: 34,
+                      backgroundColor: '#D3D3D3',
+                      borderRadius: 69,
+                      fontWeight: 500,
+                      fontSize: 40,
+                      textAlign: 'center',
+                      color: '#000000',
+                  }}
+              >
+                  <Text style={{ fontFamily: 'Apple SD Gothic Neo' }}>Add to Favorites</Text>
+              </Button>
+          </Center>
 
-        <View
-          style={{
-            backgroundColor: "white",
-            position: "absolute",
-            top: 10,
-            right: 10,
-            width: 160,
-            height: 160,
-            borderRadius: 20,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
+          <View
+              style={{
+                  position: 'absolute',
 
-            elevation: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              fontWeight: "bold",
-              color: "black",
-              textAlign: "center",
-              marginLeft: "22%",
-              top: 50,
-              position: "absolute",
-              fontFamily: "Apple SD Gothic Neo",
-            }}
+                  top: 180,
+                  width: 350,
+                  height: 350,
+                  borderRadius: 20,
+
+                  backgroundColor: '#E2ECD0',
+                  shadowColor: '#000',
+              }}
           >
-            Top Right
-          </Text>
-        </View>
+              {/* Four squarees */}
+              <View
+                  style={{
+                      backgroundColor: 'white',
+                      position: 'absolute',
+                      top: 10,
+                      left: 10,
+                      width: 160,
+                      height: 160,
+                      borderRadius: 20,
+                      shadowColor: '#000',
+                      shadowOffset: {
+                          width: 0,
+                          height: 2,
+                      },
+                      shadowOpacity: 0.25,
+                      shadowRadius: 3.84,
 
-        <View
-          style={{
-            backgroundColor: "white",
-            position: "absolute",
-            top: 180,
-            left: 10,
-            width: 160,
-            height: 160,
-            borderRadius: 20,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
+                      elevation: 5,
+                  }}
+              >
+                  <Text
+                      style={{
+                          fontSize: 20,
+                          fontWeight: 'bold',
+                          color: 'black',
+                          textAlign: 'center',
+                          marginLeft: '25%',
+                          top: 50,
+                          position: 'absolute',
+                          fontFamily: 'Apple SD Gothic Neo',
+                      }}
+                  >
+                      Top Left
+                  </Text>
+              </View>
 
-            elevation: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              fontWeight: "bold",
-              color: "black",
-              textAlign: "center",
-              marginLeft: "16%",
-              top: 50,
-              position: "absolute",
-              fontFamily: "Apple SD Gothic Neo",
-            }}
-          >
-            Bottom Left
-          </Text>
-        </View>
+              <View
+                  style={{
+                      backgroundColor: 'white',
+                      position: 'absolute',
+                      top: 10,
+                      right: 10,
+                      width: 160,
+                      height: 160,
+                      borderRadius: 20,
+                      shadowColor: '#000',
+                      shadowOffset: {
+                          width: 0,
+                          height: 2,
+                      },
+                      shadowOpacity: 0.25,
+                      shadowRadius: 3.84,
 
-        <View
-          style={{
-            backgroundColor: "white",
-            position: "absolute",
-            top: 180,
-            right: 10,
-            width: 160,
-            height: 160,
-            borderRadius: 20,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
+                      elevation: 5,
+                  }}
+              >
+                  <Text
+                      style={{
+                          fontSize: 20,
+                          fontWeight: 'bold',
+                          color: 'black',
+                          textAlign: 'center',
+                          marginLeft: '22%',
+                          top: 50,
+                          position: 'absolute',
+                          fontFamily: 'Apple SD Gothic Neo',
+                      }}
+                  >
+                      Top Right
+                  </Text>
+              </View>
 
-            elevation: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              fontWeight: "bold",
-              color: "black",
-              textAlign: "center",
-              marginLeft: "16%",
-              top: 50,
-              position: "absolute",
-              fontFamily: "Apple SD Gothic Neo",
-            }}
-          >
-            Bottom Right
-          </Text>
-        </View>
+              <View
+                  style={{
+                      backgroundColor: 'white',
+                      position: 'absolute',
+                      top: 180,
+                      left: 10,
+                      width: 160,
+                      height: 160,
+                      borderRadius: 20,
+                      shadowColor: '#000',
+                      shadowOffset: {
+                          width: 0,
+                          height: 2,
+                      },
+                      shadowOpacity: 0.25,
+                      shadowRadius: 3.84,
+
+                      elevation: 5,
+                  }}
+              >
+                  <Text
+                      style={{
+                          fontSize: 20,
+                          fontWeight: 'bold',
+                          color: 'black',
+                          textAlign: 'center',
+                          marginLeft: '16%',
+                          top: 50,
+                          position: 'absolute',
+                          fontFamily: 'Apple SD Gothic Neo',
+                      }}
+                  >
+                      Bottom Left
+                  </Text>
+              </View>
+
+              <View
+                  style={{
+                      backgroundColor: 'white',
+                      position: 'absolute',
+                      top: 180,
+                      right: 10,
+                      width: 160,
+                      height: 160,
+                      borderRadius: 20,
+                      shadowColor: '#000',
+                      shadowOffset: {
+                          width: 0,
+                          height: 2,
+                      },
+                      shadowOpacity: 0.25,
+                      shadowRadius: 3.84,
+
+                      elevation: 5,
+                  }}
+              >
+                  <Text
+                      style={{
+                          fontSize: 20,
+                          fontWeight: 'bold',
+                          color: 'black',
+                          textAlign: 'center',
+                          marginLeft: '16%',
+                          top: 50,
+                          position: 'absolute',
+                          fontFamily: 'Apple SD Gothic Neo',
+                      }}
+                  >
+                      Bottom Right
+                  </Text>
+              </View>
+          </View>
       </View>
-    </View>
-  );
+  )
 }
--- client/screens/MyClothes.js
+++ client/screens/MyClothes.js
@@ -1,123 +1,166 @@
-import "react-native-gesture-handler";
-import React, { useState, useEffect } from "react";
-import { Select, Center, Box, CheckIcon } from "native-base";
+import 'react-native-gesture-handler'
+import React, { useState, useEffect } from 'react'
+import { Select, Center, Box, CheckIcon, FlatList } from 'native-base'
 import {
-  Button,
-  Image,
-  View,
-  Platform,
-  ImageBackground,
-  TouchableOpacity,
-  Text,
-} from "react-native";
-import * as ImagePicker from "expo-image-picker";
+    Button,
+    Image,
+    View,
+    Platform,
+    ImageBackground,
+    TouchableOpacity,
+    Text,
+} from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
+import axios from 'axios'
 
 export default function MyClothes({ navigation }) {
-  const [image, setImage] = useState(null);
-  const [category, setCategory] = React.useState("");
+    const [image, setImage] = useState(null)
+    const [category, setCategory] = React.useState('')
+    const [categoryList, setCategoryList] = React.useState([])
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        })
 
-    if (!result.cancelled) {
-      setImage(result.uri);
+        if (!result.cancelled) {
+            setImage(result.uri)
+        }
     }
-  };
 
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        style={{
-          fontSize: 30,
-          fontWeight: "bold",
-          color: "black",
-          textAlign: "center",
-          top: 75,
-          position: "absolute",
-          fontFamily: "Apple SD Gothic Neo",
-        }}
-      >
-        MyClothes
-      </Text>
-      <View
-          style={{
-            top: 125,
-            left:105,
-            position: "absolute",
-            width: 165,
-            height: 30,
-            borderRadius: 10,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
+    const handleUpload = async (itemValue) => {
+        setCategory(itemValue)
+        const fileUri = image
+        const fileName = fileUri.split('/').pop()
+        try {
+            let category = itemValue
+            const response = await axios.post('http://192.168.10.27:3005/api/upload_item', {
+              upload_filename: fileUri,
+              category: category,
+              item_name: fileName,
+            })
+            console.log(response.status)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-            elevation: 5,
-          
-            backgroundColor: "#E2ECD0",
-            shadowColor: "#000",
-        }}
-      >
-      <TouchableOpacity style={{ position: "absolute"}} onPress={pickImage}>
-      <Text
-          style={{ 
-          fontSize: 12,      
-          textAlign: "center",
-          left:33,
-          top:7
-        }}
-      >
-        Upload New Clothes
-      </Text>
-        <Image
-          source={require("../../assets/uploadIcon.png")}
-          style={{
-            position: "absolute",
-            left:10,
-            top:4,
-            width: 20,
-            height: 20,
-          }}
-        />
-      </TouchableOpacity>
-      </View>
+    const getCategory = async () => {
+        try {
+            const response = await axios.get('http://192.168.10.27:3005/api/categories')
+            setCategoryList(response.data.categories)
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
+    useEffect(() => {
+        getCategory()
+    }, [])
 
-      <Text>What Category is this item?</Text>
-      <Box maxW="300" style={{ position: "absolute", top: 175 }}>
-        <Select
-          selectedValue={category}
-          minWidth="300"
-          height={50}
-          fontSize="lg"
-          placeholder="Choose A Clothing Category"
-          _selectedItem={{
-            bg: "teal.600",
-            endIcon: <CheckIcon size="5" />,
-          }}
-          mt={1}
-          onValueChange={(itemValue) => setCategory(itemValue)}
-        >
-          {/* Object containing list of items in outfit*/}
-          <Select.Item label="Hats" value="{top:top1.png or uri}" />
-          <Select.Item label="Shirts" value="{top:top2.png}" />
-          <Select.Item label="Bottoms" value="{top:top3.png}" />
-          <Select.Item label="Shoes" value="{top:top4.png}" />
-          
-        </Select>
-      </Box>
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text
+                style={{
+                    fontSize: 40,
+                    fontWeight: 'bold',
+                    color: 'black',
+                    textAlign: 'center',
+                    top: 90,
+                    position: 'absolute',
+                    fontFamily: 'Apple SD Gothic Neo',
+                }}
+            >
+                My Clothes
+            </Text>
+            <View
+                style={{
+                    top: 165,
+                    left: 105,
+                    position: 'absolute',
+                    width: 165,
+                    height: 30,
+                    borderRadius: 10,
+                    shadowColor: '#000',
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+
+                    elevation: 5,
+
+                    backgroundColor: '#E2ECD0',
+                    shadowColor: '#000',
+                }}
+            >
+                <TouchableOpacity style={{ position: 'absolute' }} onPress={pickImage}>
+                    <Text
+                        style={{
+                            fontSize: 12,
+                            textAlign: 'center',
+                            left: 40,
+                            top: 7,
+                        }}
+                    >
+                        Upload New Clothes
+                    </Text>
+                    <Image
+                        source={require('../../assets/uploadIcon.png')}
+                        style={{
+                            position: 'absolute',
+                            left: 10,
+                            top: 4,
+                            width: 20,
+                            height: 20,
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+
+            <Box maxW="300" style={{ position: 'absolute', bottom: 105 }}>
+                {image != null ? (
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            fontWeight: 'bold',
+                            color: 'black',
+                            textAlign: 'center',
+                            top: -30,
+                        }}
+                    >
+                        What Category is this Item?
+                    </Text>
+                ) : (
+                    <Text></Text>
+                )}
+                <Select
+                    selectedValue={category}
+                    minWidth="300"
+                    height={50}
+                    fontSize="lg"
+                    placeholder="Choose A Clothing Category"
+                    _selectedItem={{
+                        bg: 'teal.600',
+                        endIcon: <CheckIcon size="5" />,
+                    }}
+                    mt={1}
+                    onValueChange={(itemValue) => handleUpload(itemValue)}
+                >
+                    {categoryList.map((category, index) => (
+                        <Select.Item key={index} label={category} value={category} />
+                    ))}
+                </Select>
+            </Box>
+        </View>
+    )
 }
