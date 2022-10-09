92a6fb926383f4d2d8e30174fbe8323a38538d94
Thomas <nyumat18@gmail.com>
2022-10-08 18:01:23 -0700

    Added Login and Register Validation and UI + TabNav code split

--- App.js
+++ App.js
@@ -1,134 +1,61 @@
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { NativeBaseProvider, Box, extendTheme } from "native-base";
-import { registerRootComponent } from "expo";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import 'react-native-gesture-handler'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { NativeBaseProvider, Box, extendTheme } from 'native-base'
+import { registerRootComponent } from 'expo'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
-import DailyFit from "./client/screens/DailyFit";
-import FitFactory from "./client/screens/FitFactory";
-import MyClothes from "./client/screens/MyClothes";
-import MyFits from "./client/screens/MyFits";
-import Categories from "./client/screens/Categories";
+import TabNavigator from './client/components/TabNavigator'
 
-const Tab = createMaterialBottomTabNavigator();
+import Register from './client/screens/auth/Register'
+import Login from './client/screens/auth/Login'
+import { useState } from 'react'
+
+const Tab = createMaterialBottomTabNavigator()
 const newColorTheme = {
-  brand: {
-    900: "#8287af",
-    800: "#7c83db",
-    700: "#b3bef6",
-  },
-};
-const theme = extendTheme({ colors: newColorTheme });
-// 3. Pass the `theme` prop to the `NativeBaseProvider`
+    brand: {
+        900: '#8287af',
+        800: '#7c83db',
+        700: '#b3bef6',
+    },
+}
+const theme = extendTheme({ colors: newColorTheme })
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <NativeBaseProvider theme={theme}>
-        <Tab.Navigator
-          shifting={false}
-          barStyle={{ backgroundColor: "#DBDBDB" }}
-        >
-          <Tab.Screen
-            options={{
-              tabBarLabel: false,
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <View>
-                    <Image
-                      source={require("./assets/dailyFit.png")}
-                      resizeMode="contain"
-                      style={[
-                        {
-                          transform: [{ translateY: -15 }, { scale: 0.5 }],
-                        },
-                      ]}
-                    />
-                  </View>
-                );
-              },
-            }}
-            name="DailyFit"
-            component={DailyFit}
-          />
+    const [loggedIn, setIsLoggedIn] = useState(false)
 
-          <Tab.Screen
-            options={{
-              tabBarLabel: false,
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <View>
-                    <Image
-                      source={require("./assets/fitFactory.png")}
-                      resizeMode="contain"
-                      style={[
-                        {
-                          transform: [{ translateY: -15 }, { scale: 0.5 }],
-                        },
-                      ]}
-                    />
-                  </View>
-                );
-              },
-            }}
-            name="FitFactory"
-            component={FitFactory}
-          />
-
-          <Tab.Screen
-            options={{
-              tabBarLabel: false,
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <View>
-                    <Image
-                      source={require("./assets/myFits.png")}
-                      resizeMode="contain"
-                      style={[
-                        {
-                          transform: [{ translateY: -15 }, { scale: 0.5 }],
-                        },
-                      ]}
-                    />
-                  </View>
-                );
-              },
-            }}
-            name="MyFits"
-            component={MyFits}
-          />
-          <Tab.Screen
-            options={{
-              tabBarLabel: false,
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <View>
-                    <Image
-                      source={require("./assets/myClothes.png")}
-                      resizeMode="contain"
-                      style={[
-                        {
-                          transform: [{ translateY: -15 }, { scale: 0.5 }],
-                        },
-                      ]}
-                    />
-                  </View>
-                );
-              },
-            }}
-            name="MyClothes"
-            component={MyClothes}
-          />
-        </Tab.Navigator>
-      </NativeBaseProvider>
-      <StatusBar style="auto" />
-    </NavigationContainer>
-  );
+    if (loggedIn) {
+        return (
+            <NativeBaseProvider theme={theme}>
+                <NavigationContainer independent={true}>
+                    <TabNavigator />
+                </NavigationContainer>
+              <StatusBar style="auto" />
+            </NativeBaseProvider>
+        )
+    } else {
+        return (
+            <NativeBaseProvider theme={theme}>
+                <NavigationContainer independent={true}>
+                    <Stack.Navigator
+                        initialRouteName="Login"
+                        screenOptions={{
+                            headerShown: false,
+                        }}
+                    >
+                        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+                        <Stack.Screen name="Login" component={Login} />
+                        <Stack.Screen name="Register" component={Register} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+              <StatusBar style="auto" />
+            </NativeBaseProvider>
+        )
+    }
 }
 
-registerRootComponent(App);
+registerRootComponent(App)
